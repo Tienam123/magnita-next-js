9064e7721b59584f1d110502b8655235bca32c35
Vladyslav <dr.tienam123@gmail.com>
2025-02-06 17:20:39 +0200

    Сделал аккордеон

--- contants/index.ts
+++ contants/index.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://magnita.ua/api';
-export const API_URL = 'http://localhost/api';
+export const API_URL = 'https://magnita.ua/api';
+// export const API_URL = 'http://localhost/api';
